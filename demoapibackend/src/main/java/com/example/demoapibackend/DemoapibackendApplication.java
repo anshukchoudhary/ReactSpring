efab3bfe1a509a0f98dbb4d24582e6ca0d35081e
Code champ <anshuc52@gmail.com>
2022-01-02 00:00:46 +0530

    Backend spring initial commit

---
+++ demoapibackend/src/main/java/com/example/demoapibackend/DemoapibackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoapibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoapibackendApplication.class, args);
+	}
+
+}
---
+++ demoapibackend/src/test/java/com/example/demoapibackend/DemoapibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
