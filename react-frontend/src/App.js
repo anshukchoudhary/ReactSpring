707570bac401f1ee840421e020d437c0e53abcb0
Code champ <anshuc52@gmail.com>
2022-01-02 01:55:37 +0530

    added url change

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. I am there.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:9092/baseMapping/getDetails"
           target="_blank"
           rel="noopener noreferrer"
         >
